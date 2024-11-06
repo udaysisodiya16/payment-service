0d47e8d1ad811dd6ec9d8788bfce7b62ea316d32
Uday Sisodiya <uday.sisodiya@agshealth.com>
2024-11-06 17:15:12 +0530

    Initial commit

---
+++ src/test/java/com/capstone/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
